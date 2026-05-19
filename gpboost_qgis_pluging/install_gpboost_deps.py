21581c727774a3a4654f4ce6fcc59efda8fe8c20
Jesús Enrique Flores-Riera <jfloresr@unal.edu.co>
2026-05-19 00:01:29 -0400

    Delete gpboost_qgis_pluging directory

--- gpboost_qgis_pluging/install_gpboost_deps.py
+++
@@ -1,9 +0,0 @@
-"""
-Ejecuta desde la consola Python de QGIS:
-  exec(open("/ruta/plugin/install_gpboost_deps.py").read())
-"""
-import subprocess, sys
-
-print("Instalando gpboost...")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "gpboost>=1.4.0"])
-print("✅ gpboost instalado. Reinicia QGIS.")
